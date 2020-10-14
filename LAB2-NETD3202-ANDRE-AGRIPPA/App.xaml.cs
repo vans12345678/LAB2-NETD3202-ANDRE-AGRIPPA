644b97f2770a4f1a577e4e55ceff08612599e1b3
home <andreagrippa76@gmail.com>
2020-10-14 19:49:13 -0400

    Add project files.

---
+++ LAB2-NETD3202-ANDRE-AGRIPPA/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LAB2_NETD3202_ANDRE_AGRIPPA
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

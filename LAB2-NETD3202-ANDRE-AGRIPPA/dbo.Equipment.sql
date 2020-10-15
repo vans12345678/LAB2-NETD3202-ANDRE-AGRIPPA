490190d69bfb25f6362a40e032027cf820e4b0c5
home <andreagrippa76@gmail.com>
2020-10-14 20:03:16 -0400

    Created Equipment db

---
+++ LAB2-NETD3202-ANDRE-AGRIPPA/dbo.Equipment.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Equipment]
+(
+	[name] NVARCHAR(MAX) NOT NULL,
+	[empID] INT NOT NULL, 
+    [desc] NVARCHAR(MAX) NOT NULL, 
+    [phone] NVARCHAR(MAX) NOT NULL, 
+    CONSTRAINT [PK_Equipment] PRIMARY KEY ([empID]),
+
+)

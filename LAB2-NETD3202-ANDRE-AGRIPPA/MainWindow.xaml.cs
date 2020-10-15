1f0c3aead16b55e334ccc97f62839e686415bf7f
home <andreagrippa76@gmail.com>
2020-10-14 21:51:50 -0400

    Add button makes new record

--- LAB2-NETD3202-ANDRE-AGRIPPA/MainWindow.xaml.cs
+++ LAB2-NETD3202-ANDRE-AGRIPPA/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,5 +30,59 @@ namespace LAB2_NETD3202_ANDRE_AGRIPPA
         {
 
         }
+
+        private void AddEquipmentToDatabase(object sender, RoutedEventArgs e)
+        {
+            // try
+            // {
+            //     string connectString = Properties.Settings.Default.connect_string;
+            //
+            //     SqlConnection conn = new SqlConnection(connectString);
+            //     conn.Open();
+            //
+            //     string insertQuery = "INSERT INTO Equipment (name, empID, desc, phone) VALUES" +
+            //                          "('" + txtName.Text + "', '" + txtEmployeeID.Text + "', '" + txtDescOfEquipment.Text + "', '" + txtContactPhoneNumber.Text + "')";
+            //
+            //     SqlCommand command = new SqlCommand(insertQuery, conn);
+            //
+            //     command.ExecuteNonQuery();
+            //     conn.Close();
+            //
+            //     MessageBox.Show("Successfully added equipment entry");
+            // }
+            // catch (Exception exception)
+            // {
+            //     Console.WriteLine(exception);
+            //     throw;
+            // }
+            
+        }
+
+        private void btnAddToDatabase_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string connectString = Properties.Settings.Default.connect_string;
+
+                SqlConnection conn = new SqlConnection(connectString);
+                conn.Open();
+
+                string insertQuery = "INSERT INTO [Equipment] (name, empID, description, phone) VALUES('" + txtName.Text + "', '" + int.Parse(txtEmployeeID.Text) + "', '" + txtDescOfEquipment.Text + "', '" + txtContactPhoneNumber.Text + "')";
+
+                SqlCommand command = new SqlCommand(insertQuery, conn);
+
+                command.ExecuteNonQuery();
+                conn.Close();
+
+                MessageBox.Show("Successfully added equipment entry");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+
+        }
     }
-}
+    }
+
